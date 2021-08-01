4dfac568ed9c304c28b72980edc567729bd1e5f8
Gabriel Gardini <g246289@dac.unicamp.br>
2021-08-01 12:29:38 -0300

    Adicionados botões para gravação das teclas pressionadas

--- backup.py
+++ backup.py
@@ -1,258 +1,158 @@
 from tkinter import *
 import pygame
+from threading import Thread
+import time 
+import sys
 
-janela = Tk()
-janela.title("Piano Python")
-janela.geometry("775x400")
-janela.maxsize(775,400)
-janela.configure(background="#03BB85")
-
+main = Tk()
+main.title("Piano Python")
+main.geometry("775x400")
+main.maxsize(775,400)
+main.configure(background="#03BB85")
 pygame.mixer.init()
 
-def play_c3():
-    pygame.mixer.music.load("sons\c3.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_d3():
-    pygame.mixer.music.load("sons\\d3.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_e3():
-    pygame.mixer.music.load("sons\e3.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_f3():
-    pygame.mixer.music.load("sons\\f3.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_g3():
-    pygame.mixer.music.load("sons\g3.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_a4():
-    pygame.mixer.music.load("sons\\a4.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_b4():
-    pygame.mixer.music.load("sons\\b4.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_c4():
-    pygame.mixer.music.load("sons\c4.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_d4():
-    pygame.mixer.music.load("sons\\d4.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_e4():
-    pygame.mixer.music.load("sons\e4.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_f4():
-    pygame.mixer.music.load("sons\\f4.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_g4():
-    pygame.mixer.music.load("sons\g4.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_a5():
-    pygame.mixer.music.load("sons\\a5.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_b5():
-    pygame.mixer.music.load("sons\\b5.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_c5():
-    pygame.mixer.music.load("sons\c5.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_d5():
-    pygame.mixer.music.load("sons\\d5.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_e5():
-    pygame.mixer.music.load("sons\e5.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_f5():
-    pygame.mixer.music.load("sons\\f5.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_g5():
-    pygame.mixer.music.load("sons\g5.mp3")
-    pygame.mixer.music.play(loops=0)
-
-
-
-#teclas pretas
-
-def play_c_3():
-    pygame.mixer.music.load("sons\c-3.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_d_3():
-    pygame.mixer.music.load("sons\\d-3.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_f_3():
-    pygame.mixer.music.load("sons\\f-3.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_g_3():
-    pygame.mixer.music.load("sons\g-3.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_a_4():
-    pygame.mixer.music.load("sons\\a-4.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_c_4():
-    pygame.mixer.music.load("sons\c-4.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_d_4():
-    pygame.mixer.music.load("sons\\d-4.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_f_4():
-    pygame.mixer.music.load("sons\\f-4.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_g_4():
-    pygame.mixer.music.load("sons\g-4.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_a_5():
-    pygame.mixer.music.load("sons\\a-5.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_c_5():
-    pygame.mixer.music.load("sons\c-5.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_d_5():
-    pygame.mixer.music.load("sons\\d-5.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_f_5():
-    pygame.mixer.music.load("sons\\f-5.mp3")
-    pygame.mixer.music.play(loops=0)
-
-def play_g_5():
-    pygame.mixer.music.load("sons\g-5.mp3")
-    pygame.mixer.music.play(loops=0)
-
-
-
-#Teclas Brancas
-c3 = Button(janela, text="Dó3", command=play_c3, anchor=S)
-c3.place(x=0,y=150, width=40, height=250)
-
-d3 = Button(janela, text="Ré",command=play_d3, anchor=S)
-d3.place(x=40,y=150, width=40, height=250)
-
-e3 = Button(janela, text="Mi",command=play_e3, anchor=S)
-e3.place(x=80,y=150, width=40, height=250)
-
-f3 = Button(janela, text="Fá",command=play_f3, anchor=S)
-f3.place(x=120,y=150, width=40, height=250)
-
-g3 = Button(janela, text="Sol",command=play_g3, anchor=S)
-g3.place(x=160,y=150, width=40, height=250)
-
-a4 = Button(janela, text="Lá",command=play_a4, anchor=S)
-a4.place(x=200,y=150, width=40, height=250)
-
-b4 = Button(janela, text="Si",command=play_b4, anchor=S)
-b4.place(x=240,y=150, width=40, height=250)
-
-c4 = Button(janela, text="Dó4",command=play_c4, anchor=S)
-c4.place(x=280,y=150, width=40, height=250)
-
-d4 = Button(janela, text="Ré",command=play_d4, anchor=S)
-d4.place(x=320,y=150, width=40, height=250)
-
-e4 = Button(janela, text="Mi",command=play_e4, anchor=S)
-e4.place(x=360,y=150, width=40, height=250)
-
-f4 = Button(janela, text="Fá",command=play_f4, anchor=S)
-f4.place(x=400,y=150, width=40, height=250)
-
-g4 = Button(janela, text="Sol",command=play_g4, anchor=S)
-g4.place(x=440,y=150, width=40, height=250)
-
-a5 = Button(janela, text="Lá",command=play_a5, anchor=S)
-a5.place(x=480,y=150, width=40, height=250)
-
-b5 = Button(janela, text="Si",command=play_b5, anchor=S)
-b5.place(x=520,y=150, width=40, height=250)
-
-c5 = Button(janela, text="Dó5",command=play_c5, anchor=S)
-c5.place(x=560,y=150, width=40, height=250)
-
-d5 = Button(janela, text="Ré",command=play_d5, anchor=S)
-d5.place(x=600,y=150, width=40, height=250)
-
-e5 = Button(janela, text="Mi",command=play_e5, anchor=S)
-e5.place(x=640,y=150, width=40, height=250)
-
-f5 = Button(janela, text="Fá",command=play_f5, anchor=S)
-f5.place(x=680,y=150, width=40, height=250)
-
-g5 = Button(janela, text="Sol",command=play_g5, anchor=S)
-g5.place(x=720,y=150, width=40, height=250)
-
-#Teclas Pretas
-
-c_3 = Button(janela, text="Dó#", bg="black", fg="white",command=play_c_3, anchor=S)
-c_3.place(x=25,y=150, width=30, height=180)
-
-d_3 = Button(janela, text="Ré#", bg="black", fg="white",command=play_d_3, anchor=S)
-d_3.place(x=65,y=150, width=30, height=180)
-
-f_3 = Button(janela, text="Fá#", bg="black", fg="white",command=play_f_3, anchor=S)
-f_3.place(x=145,y=150, width=30, height=180)
-
-g_3 = Button(janela, text="Sol#", bg="black", fg="white",command=play_g_3, anchor=S)
-g_3.place(x=185,y=150, width=30, height=180)
-
-a_4 = Button(janela, text="Lá#", bg="black", fg="white",command=play_a_4, anchor=S)
-a_4.place(x=225,y=150, width=30, height=180)
-
-c_4 = Button(janela, text="Dó#", bg="black", fg="white",command=play_c_4, anchor=S)
-c_4.place(x=305,y=150, width=30, height=180)
-
-d_4 = Button(janela, text="Ré#", bg="black", fg="white",command=play_d_4, anchor=S)
-d_4.place(x=345,y=150, width=30, height=180)
-
-f_4 = Button(janela, text="Fá#", bg="black", fg="white",command=play_f_4, anchor=S)
-f_4.place(x=425,y=150, width=30, height=180)
-
-g_4 = Button(janela, text="Sol#", bg="black", fg="white",command=play_g_4, anchor=S)
-g_4.place(x=465,y=150, width=30, height=180)
-
-a_5 = Button(janela, text="Lá#", bg="black", fg="white",command=play_a_5, anchor=S)
-a_5.place(x=505,y=150, width=30, height=180)
-
-c_5 = Button(janela, text="Dó#", bg="black", fg="white",command=play_c_5, anchor=S)
-c_5.place(x=585,y=150, width=30, height=180)
-
-d_5 = Button(janela, text="Ré#", bg="black", fg="white",command=play_d_5, anchor=S)
-d_5.place(x=625,y=150, width=30, height=180)
-
-f_5 = Button(janela, text="Fá#", bg="black", fg="white",command=play_f_5, anchor=S)
-f_5.place(x=705,y=150, width=30, height=180)
-
-g_5 = Button(janela, text="Sol#", bg="black", fg="white",command=play_g_5, anchor=S)
-g_5.place(x=745,y=150, width=30, height=180)
-
-apresentar=Label(janela,text="Bem vindo ao Piano Virtual! \n Obrigado pela preferência :) \n Criado por Gabriel Gardini \n @ggardini1",font=("Helvetica",15),bg= "#03BB85",fg="white")
-apresentar.place(x=188,y=25,width=400,height=100)
-
-tapa_buraco=Button(janela)
-tapa_buraco.place(x=760,y=330,width=30,height=70)
-
-janela.mainloop()
+class Piano:
+    def __init__ (self, main):
+        self.main = main
+        self.teclas = dict()
+        self.pretas_temp = dict()
+
+        
+
+        self.teclas_para_reproduzir = [] # lista q ira armazenar as notas q estão sendo gravadas
+
+        ## Display
+
+        self.apresentar=Label(main,text="Bem vindo ao Piano Virtual! \n Obrigado pela preferência :) \n Criado por Gabriel Gardini \n @ggardini1",font=("Helvetica",15),bg= "#03BB85",fg="white")
+        self.apresentar.place(x=188,y=25,width=400,height=100)
+
+        ## Recording
+
+        self.is_recording = False
+
+        self.botao_gravar_img=PhotoImage(file="imagens\gravar.png")
+        self.img_label_gravar=Label(image=self.botao_gravar_img)
+
+        self.botao_parar_img=PhotoImage(file="imagens\parar.png")
+        self.img_label_parar=Label(image=self.botao_parar_img)
+        
+
+        self.botao_gravar = Button(main, image=self.botao_gravar_img, command=self.record)
+        self.botao_gravar.place(x=725,y=30)
+        
+        
+        ## Play recorded button
+
+        self.botao_play_img=PhotoImage(file="imagens\play.png")
+        self.img_label_play=Label(image=self.botao_parar_img)
+
+        self.botao_play = Button(main, image=self.botao_play_img, command=self.play_recorded, state='disabled')
+        self.botao_play.place(x=725, y=80)
+
+        self.nome_notas = [
+            "Dó3", "Ré", "Mi", "Fá", "Sol","Lá","Si",
+            "Dó4","Ré","Mi", "Fá", "Sol", "Lá", "Si",
+            "Dó5", "Ré", "Mi", "Fá", "Sol", " ",
+            ]
+        
+        self.nome_notas_numerada = [
+            "c3", "d3", "e3", "f3", "g3","a4","b4",
+            "c4","d4","e4", "f4", "g4", "a5", "b5",
+            "c5", "d5", "e5", "f5", "g5", " ",
+            ]
+        
+        self.notas_pretas = [
+            "Dó#", "Ré#", "Fá#", "Sol#","Lá#",
+            "Dó#","Ré#", "Fá#", "Sol#", "Lá#",
+            "Dó#", "Ré#", "Fá#", "Sol#", " ",
+            ]
+
+        self.notas_pretas_numeradas = [
+            "c-3", "d-3", "f-3", "g-3","a-4",
+            "c-4","d-4", "f-4", "g-4", "a-5",
+            "c-5", "d-5", "f-5", "g-5", " ",
+            ]
+
+        ## Teclas brancas
+
+        for i in range(20):
+            self.tecla = Button(main, text=self.nome_notas[i], anchor=S)
+            self.tecla.place(x=i*40, y=150, width=40, height=250)
+            self.teclas[self.tecla] = self.nome_notas_numerada[i]
+            self.tecla.bind('<Button-1>', self.tocar)
+
+        ## Teclas Pretas
+
+        aux = 25
+        count = -1
+
+        for i in range(1, 7):
+
+            if i % 2 == 0:
+                u = 3
+            else:
+                u = 2
+
+            for j in range(u):
+                count +=1 
+                self.tecla_preta = Button(main, text=f't{j}',bg='black', fg='white', anchor=S)
+                self.tecla_preta.place(x=aux, y=150, width=30, height=180)
+                self.pretas_temp[f'tecla {count}'] = self.tecla_preta
+                aux += 40
+
+            aux += 40
+
+        ## nomeia teclas pretas
+
+        for i in range(len(self.notas_pretas)):
+            self.t = self.pretas_temp[f'tecla {i}']
+            self.t.bind('<Button-1>', self.tocar)
+            self.teclas[self.t] = self.notas_pretas_numeradas[i]
+            self.t['text'] = self.notas_pretas[i]
+
+
+    def tocar(self, event):
+        nota = self.teclas[event.widget]
+        pygame.mixer.music.load(f"sons\{nota}.mp3")
+        pygame.mixer.music.play(loops=0)
+        if self.is_recording:
+            self.teclas_para_reproduzir.append(nota)
+            print('recebi', nota)
+
+    def record(self):
+        self.is_recording = True
+        self.botao_gravar.config(image=self.botao_parar_img, command=self.stop_recording, relief='sunken')
+        self.teclas_para_reproduzir = []
+        self.play_button_is_active = False
+        self.botao_play.config(state='disabled')
+
+
+    def stop_recording(self):
+        self.is_recording = False
+        self.botao_gravar.config(image=self.botao_gravar_img, command=self.record, relief='raised')
+        if self.teclas_para_reproduzir != []:
+            self.botao_play.config(state='normal')
+
+
+    def play_recorded(self):
+        playlist = []
+        for nota in self.teclas_para_reproduzir:
+            playlist.append(f"sons\{nota}.mp3")
+        
+        self.run_playlist(playlist, 0)
+
+    def run_playlist(self, playlist, index):
+        if index < len(playlist):
+            self.botao_play.config(state='disabled')
+            self.botao_gravar.config(state='disabled')
+            pygame.mixer.music.load(playlist[index])
+            pygame.mixer.music.play()
+            self.main.after(500, lambda pl = playlist, i = index+1: self.run_playlist(pl, i))
+        else:
+            self.botao_play.config(state='normal')
+            self.botao_gravar.config(state='normal')
+
+
+j = Piano(main)
+
+main.mainloop()
--- piano.py
+++ piano.py
@@ -1,80 +1,158 @@
 from tkinter import *
 import pygame
+from threading import Thread
+import time 
+import sys
 
-janela = Tk()
-janela.title("Piano Python")
-janela.geometry("775x400")
-janela.maxsize(775,400)
-janela.configure(background="#03BB85")
-
+main = Tk()
+main.title("Piano Python")
+main.geometry("775x400")
+main.maxsize(775,400)
+main.configure(background="#03BB85")
 pygame.mixer.init()
 
-teclas = dict()
+class Piano:
+    def __init__ (self, main):
+        self.main = main
+        self.teclas = dict()
+        self.pretas_temp = dict()
 
-pretas_temp = dict()
+        
 
-nome_notas = [
-    "Dó3", "Ré", "Mi", "Fá", "Sol","Lá","Si",
-    "Dó4","Ré","Mi", "Fá", "Sol", "Lá", "Si",
-    "Dó5", "Ré", "Mi", "Fá", "Sol", " ",
-    ]
+        self.teclas_para_reproduzir = [] # lista q ira armazenar as notas q estão sendo gravadas
 
-nome_notas_numerada = [
-    "c3", "d3", "e3", "f3", "g3","a4","b4",
-    "c4","d4","e4", "f4", "g4", "a5", "b5",
-    "c5", "d5", "e5", "f5", "g5", " ",
-    ]
+        ## Display
 
-notas_pretas = [
-    "Dó#", "Ré#", "Fá#", "Sol#","Lá#",
-    "Dó#","Ré#", "Fá#", "Sol#", "Lá#",
-    "Dó#", "Ré#", "Fá#", "Sol#", " ",
-    ]
+        self.apresentar=Label(main,text="Bem vindo ao Piano Virtual! \n Obrigado pela preferência :) \n Criado por Gabriel Gardini \n @ggardini1",font=("Helvetica",15),bg= "#03BB85",fg="white")
+        self.apresentar.place(x=188,y=25,width=400,height=100)
 
-notas_pretas_numeradas = [
-    "c-3", "d-3", "f-3", "g-3","a-4",
-    "c-4","d-4", "f-4", "g-4", "a-5",
-    "c-5", "d-5", "f-5", "g-5", " ",
-    ]
+        ## Recording
 
-def tocar_branco(event):
-    nota = teclas[event.widget]
-    pygame.mixer.music.load(f"sons\{nota}.mp3")
-    pygame.mixer.music.play(loops=0)
+        self.is_recording = False
 
-for i in range(20):
-    tecla = Button(janela, text=nome_notas[i], anchor=S)
-    tecla.place(x=i*40, y=150, width=40, height=250)
-    teclas[tecla] = nome_notas_numerada[i]
-    tecla.bind('<Button-1>', tocar_branco)
+        self.botao_gravar_img=PhotoImage(file="imagens\gravar.png")
+        self.img_label_gravar=Label(image=self.botao_gravar_img)
 
-# Teclas Pretas
+        self.botao_parar_img=PhotoImage(file="imagens\parar.png")
+        self.img_label_parar=Label(image=self.botao_parar_img)
+        
 
-aux = 25
-count = -1
+        self.botao_gravar = Button(main, image=self.botao_gravar_img, command=self.record)
+        self.botao_gravar.place(x=725,y=30)
+        
+        
+        ## Play recorded button
 
-for i in range(1, 7):
+        self.botao_play_img=PhotoImage(file="imagens\play.png")
+        self.img_label_play=Label(image=self.botao_parar_img)
 
-    if i % 2 == 0:
-        u = 3
-    else:
-        u = 2
+        self.botao_play = Button(main, image=self.botao_play_img, command=self.play_recorded, state='disabled')
+        self.botao_play.place(x=725, y=80)
 
-    for j in range(u):
-        count +=1 
-        tecla_preta = Button(janela, text=f't{j}',bg='black', fg='white', anchor=S)
-        tecla_preta.place(x=aux, y=150, width=30, height=180)
-        pretas_temp[f'tecla {count}'] = tecla_preta
-        aux += 40
+        self.nome_notas = [
+            "Dó3", "Ré", "Mi", "Fá", "Sol","Lá","Si",
+            "Dó4","Ré","Mi", "Fá", "Sol", "Lá", "Si",
+            "Dó5", "Ré", "Mi", "Fá", "Sol", " ",
+            ]
+        
+        self.nome_notas_numerada = [
+            "c3", "d3", "e3", "f3", "g3","a4","b4",
+            "c4","d4","e4", "f4", "g4", "a5", "b5",
+            "c5", "d5", "e5", "f5", "g5", " ",
+            ]
+        
+        self.notas_pretas = [
+            "Dó#", "Ré#", "Fá#", "Sol#","Lá#",
+            "Dó#","Ré#", "Fá#", "Sol#", "Lá#",
+            "Dó#", "Ré#", "Fá#", "Sol#", " ",
+            ]
 
-    aux += 40
+        self.notas_pretas_numeradas = [
+            "c-3", "d-3", "f-3", "g-3","a-4",
+            "c-4","d-4", "f-4", "g-4", "a-5",
+            "c-5", "d-5", "f-5", "g-5", " ",
+            ]
 
-for i in range(len(notas_pretas)):
-    t = pretas_temp[f'tecla {i}']
-    t.bind('<Button-1>', tocar_branco)
-    teclas[t] = notas_pretas_numeradas[i]
-    t['text'] = notas_pretas[i]
+        ## Teclas brancas
 
-apresentar=Label(janela,text="Bem vindo ao Piano Virtual! \n Obrigado pela preferência :) \n Criado por Gabriel Gardini \n @ggardini1",font=("Helvetica",15),bg= "#03BB85",fg="white")
-apresentar.place(x=188,y=25,width=400,height=100)
-janela.mainloop()
+        for i in range(20):
+            self.tecla = Button(main, text=self.nome_notas[i], anchor=S)
+            self.tecla.place(x=i*40, y=150, width=40, height=250)
+            self.teclas[self.tecla] = self.nome_notas_numerada[i]
+            self.tecla.bind('<Button-1>', self.tocar)
+
+        ## Teclas Pretas
+
+        aux = 25
+        count = -1
+
+        for i in range(1, 7):
+
+            if i % 2 == 0:
+                u = 3
+            else:
+                u = 2
+
+            for j in range(u):
+                count +=1 
+                self.tecla_preta = Button(main, text=f't{j}',bg='black', fg='white', anchor=S)
+                self.tecla_preta.place(x=aux, y=150, width=30, height=180)
+                self.pretas_temp[f'tecla {count}'] = self.tecla_preta
+                aux += 40
+
+            aux += 40
+
+        ## nomeia teclas pretas
+
+        for i in range(len(self.notas_pretas)):
+            self.t = self.pretas_temp[f'tecla {i}']
+            self.t.bind('<Button-1>', self.tocar)
+            self.teclas[self.t] = self.notas_pretas_numeradas[i]
+            self.t['text'] = self.notas_pretas[i]
+
+
+    def tocar(self, event):
+        nota = self.teclas[event.widget]
+        pygame.mixer.music.load(f"sons\{nota}.mp3")
+        pygame.mixer.music.play(loops=0)
+        if self.is_recording:
+            self.teclas_para_reproduzir.append(nota)
+            print('recebi', nota)
+
+    def record(self):
+        self.is_recording = True
+        self.botao_gravar.config(image=self.botao_parar_img, command=self.stop_recording, relief='sunken')
+        self.teclas_para_reproduzir = []
+        self.play_button_is_active = False
+        self.botao_play.config(state='disabled')
+
+
+    def stop_recording(self):
+        self.is_recording = False
+        self.botao_gravar.config(image=self.botao_gravar_img, command=self.record, relief='raised')
+        if self.teclas_para_reproduzir != []:
+            self.botao_play.config(state='normal')
+
+
+    def play_recorded(self):
+        playlist = []
+        for nota in self.teclas_para_reproduzir:
+            playlist.append(f"sons\{nota}.mp3")
+        
+        self.run_playlist(playlist, 0)
+
+    def run_playlist(self, playlist, index):
+        if index < len(playlist):
+            self.botao_play.config(state='disabled')
+            self.botao_gravar.config(state='disabled')
+            pygame.mixer.music.load(playlist[index])
+            pygame.mixer.music.play()
+            self.main.after(500, lambda pl = playlist, i = index+1: self.run_playlist(pl, i))
+        else:
+            self.botao_play.config(state='normal')
+            self.botao_gravar.config(state='normal')
+
+
+j = Piano(main)
+
+main.mainloop()
